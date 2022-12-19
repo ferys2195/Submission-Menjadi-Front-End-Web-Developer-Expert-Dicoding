4d0c6f15cd85795ea71031e44059479fafce7e6f
fery21irawan <fery21irawan.me@gmail.com>
2022-12-19 19:42:06 +0700

    implementation Webpack PWA Manifest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -26,6 +27,25 @@ module.exports = merge(common, {
     new WorkboxWebpackPlugin.InjectManifest({
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
+    }),
+    new WebpackPwaManifest({
+      filename: 'manifest.json',
+      id: 'indoresto-pwa-v1',
+      name: 'Indoresto',
+      short_name: 'Indoresto',
+      theme_color: '#459f2e',
+      background_color: '#EEEEEE',
+      start_url: 'index.html',
+      display: 'standalone',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/public/icons/icon-512x512.png'),
+          destination: path.join('icons'),
+          purpose: 'maskable any',
+          sizes: [48, 72, 96, 128, 144, 152, 192, 256, 284, 384, 512], // multiple sizes
+        },
+      ],
     }),
   ],
 });
